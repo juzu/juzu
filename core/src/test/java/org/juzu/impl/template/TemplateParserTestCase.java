e236b1d10973bd60a734e0b39cb3b4c8b49df7a2
Julien Viet <julien@julienviet.com>
2011-10-19 00:32:45 +0200

    update copyright

--- core/src/test/java/org/juzu/impl/template/TemplateParserTestCase.java
+++ core/src/test/java/org/juzu/impl/template/TemplateParserTestCase.java
@@ -16,6 +16,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
 package org.juzu.impl.template;
 
 import org.juzu.impl.utils.Builder;
