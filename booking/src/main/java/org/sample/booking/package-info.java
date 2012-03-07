707eea9a90868fb15e9753f498a1a9da4a408d31
Julien Viet <julien@julienviet.com>
2012-03-07 16:50:09 +0100

    need to put booking.js after jquery in booking app

--- booking/src/main/java/org/sample/booking/package-info.java
+++ booking/src/main/java/org/sample/booking/package-info.java
@@ -25,9 +25,9 @@
    })
 @Assets(
    scripts = {
-      @Script(src = "public/javascripts/booking.js"),
       @Script(src = "public/javascripts/jquery-1.7.1.min.js"),
-      @Script(src = "public/javascripts/jquery-ui-1.7.2.custom.min.js")
+      @Script(src = "public/javascripts/jquery-ui-1.7.2.custom.min.js"),
+      @Script(src = "public/javascripts/booking.js")
    },
    stylesheets = {
       @Stylesheet(src = "public/stylesheets/main.css")
