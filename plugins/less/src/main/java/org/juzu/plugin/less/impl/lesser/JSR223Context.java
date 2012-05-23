cd1d0beb0fae4dc7b20b445d34f2ec2f8974580d
Julien Viet <julien@julienviet.com>
2012-05-23 23:14:03 +0200

    cosmetic

--- plugins/less/src/main/java/org/juzu/plugin/less/impl/lesser/JSR223Context.java
+++ plugins/less/src/main/java/org/juzu/plugin/less/impl/lesser/JSR223Context.java
@@ -9,25 +9,23 @@ public class JSR223Context extends JSContext
 {
 
    /** . */
-   private final ScriptEngine tmp = new ScriptEngineManager().getEngineByName("JavaScript");
+   private final ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
 
    @Override
    public void put(String name, Object value)
    {
-      tmp.put(name, value);
+      engine.put(name, value);
    }
 
    @Override
    public Object eval(String script) throws Exception
    {
-
-
-      return tmp.eval(script);
+      return engine.eval(script);
    }
 
    @Override
    public Object invokeFunction(String name, Object... args) throws Exception
    {
-      return ((Invocable)tmp).invokeFunction(name, args);
+      return ((Invocable)engine).invokeFunction(name, args);
    }
 }
