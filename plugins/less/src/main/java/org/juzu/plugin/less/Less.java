d9a312bb7f8cbdac55241a970b3a6bfa86448b1c
Julien Viet <julien@julienviet.com>
2012-05-28 11:06:30 +0200

    update javadoc for Less annotation

--- plugins/less/src/main/java/org/juzu/plugin/less/Less.java
+++ plugins/less/src/main/java/org/juzu/plugin/less/Less.java
@@ -5,14 +5,30 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a> */
+/**
+ * This annotation trigger declares a less stylesheet. It triggers the compilation of a less stylesheet when it is processed.
+ * by the compiler.
+ *
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.PACKAGE)
 public @interface Less
 {
 
+   /**
+    * The path of the less stylesheet to compile, relative to the <code>assets</code> application package.
+    *
+    * @return the stylesheet paths
+    */
    String[] value();
 
+   /**
+    * Configure the minification of the stylesheet when they are processed. The stylesheets are not minified
+    * by default.
+    *
+    * @return true if the stylesheets should be minidfied
+    */
    boolean minify() default false;
 
 }
