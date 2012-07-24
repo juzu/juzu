323d981169762b4b5865af1ac9da7fba7c472a90
Julien Viet <julien@julienviet.com>
2012-07-24 22:52:54 +0200

    update copyright

--- core/src/main/java/juzu/impl/router/ValueResolverFactory.java
+++ core/src/main/java/juzu/impl/router/ValueResolverFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 eXo Platform SAS.
+ * Copyright (C) 2012 eXo Platform SAS.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
