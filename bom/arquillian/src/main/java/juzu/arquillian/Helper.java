23cabb5b8386a7ee45a3e844a129b23feef9a811
Julien Viet <julien@julienviet.com>
2013-06-21 13:27:18 +0200

    Improved the test Helper

--- bom/arquillian/src/main/java/juzu/arquillian/Helper.java
+++ bom/arquillian/src/main/java/juzu/arquillian/Helper.java
@@ -20,13 +20,15 @@
 package juzu.arquillian;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 /**
  * An helper class that provides helper methods for setting up an Arquillian based test for Juzu.
@@ -49,35 +51,99 @@ public class Helper {
    * Create a base servlet war file. The returned war file is configured for running the Juzu
    * servlet and the specified injector provider.
    *
-   * @param injectorVendor the injector vendor
+   * @param injectorVendor the dependency injection vendor to use
    * @return the base servlet deployment
    */
   public static WebArchive createBaseServletDeployment(String injectorVendor) {
-    return createBaseDeployment("servlet/web.xml", injectorVendor);
+    return createBaseDeployment(null, "servlet/web.xml", injectorVendor);
   }
 
   /**
-   * Create a base portlet war file. The returned war file is configured for running the GateIn
-   * embedded portlet container.
+   * Create a portlet war file. The returned war file is configured for running the GateIn
+   * embedded portlet container. The returned deployment will contain the class hierarchy in
+   * the package of the specified <code>baseClass</code> argument.
    *
-   * @return the base portlet deployment
+   * @param baseClasses the base classes
+   * @return the portlet deployment
    */
-  public static WebArchive createBasePortletDeployment(String injectorVendor) {
-    return createBaseDeployment("portlet/web.xml", injectorVendor);
+  public static WebArchive createBasePortletDeployment(Class... baseClasses) {
+    return createBasePortletDeployment((WebArchive)null, baseClasses);
   }
 
   /**
-   * Create a base portlet war file. The returned war file is configured for running the GateIn
-   * embedded portlet container.
+   * Create a portlet war file. The returned war file is configured for running the GateIn
+   * embedded portlet container. The returned deployment will contain the class hierarchy in
+   * the package of the specified <code>baseClass</code> argument.
    *
-   * @return the base portlet deployment
+   * @param war the web archive to use
+   * @param baseClasses the base classes
+   * @return the portlet deployment
    */
-  public static WebArchive createBasePortletDeployment() {
-    return createBasePortletDeployment("weld");
+  public static WebArchive createBasePortletDeployment(WebArchive war, Class... baseClasses) {
+    return createBasePortletDeployment(war, "weld", baseClasses);
   }
 
-  private static WebArchive createBaseDeployment(String webXMLPath, String injectorProvider) {
-    WebArchive war = ShrinkWrap.create(WebArchive.class);
+  /**
+   * Create a portlet war file. The returned war file is configured for running the GateIn
+   * embedded portlet container. The returned deployment will contain the class hierarchy in
+   * the package of the specified <code>baseClass</code> argument.
+   *
+   * @param injectorVendor the dependency injection vendor to use
+   * @param baseClasses the base classes
+   * @return the portlet deployment
+   */
+  public static WebArchive createBasePortletDeployment(String injectorVendor, Class... baseClasses) {
+    return createBasePortletDeployment(null, injectorVendor, baseClasses);
+  }
+
+  /**
+   * Create a portlet war file. The returned war file is configured for running the GateIn
+   * embedded portlet container. The returned deployment will contain the class hierarchy in
+   * the package of the specified <code>baseClass</code> argument.
+   *
+   * @param war the web archive to use
+   * @param injectorVendor the dependency injection vendor to use
+   * @param baseClasses the base classes
+   * @return the portlet deployment
+   */
+  public static WebArchive createBasePortletDeployment(WebArchive war, String injectorVendor, Class... baseClasses) {
+    WebArchive deployment = createBaseDeployment(war, "portlet/web.xml", injectorVendor);
+    for (Class<?> baseClass : baseClasses) {
+      try {
+        URL root = baseClass.getClassLoader().getResource(baseClass.getName().replace('.', '/') + ".class");
+        if (root != null) {
+          File f = new File(root.toURI()).getParentFile();
+          StringBuilder path = new StringBuilder(baseClass.getPackage().getName().replace('.', '/'));
+          add(deployment, f, path);
+        }
+      }
+      catch (URISyntaxException e) {
+        throw new AssertionError("Could not create portlet deployment for class " + baseClass.getName(), e);
+      }
+    }
+    return deployment;
+  }
+
+  private static void add(WebArchive war, File f, StringBuilder path) {
+    if (f.isDirectory()) {
+      File[] children = f.listFiles();
+      if (children != null) {
+        for (File child : children) {
+          int length = path.length();
+          path.append('/').append(child.getName());
+          add(war, child, path);
+          path.setLength(length);
+        }
+      }
+    } else {
+      war.addAsResource(f, path.toString());
+    }
+  }
+
+  private static WebArchive createBaseDeployment(WebArchive war, String webXMLPath, String injectorProvider) {
+    if (war == null) {
+      war = ShrinkWrap.create(WebArchive.class);
+    }
     byte[] buffer = new byte[512];
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     InputStream in = Helper.class.getResourceAsStream(webXMLPath);
--- booking/src/test/java/org/sample/booking/AbstractBookingTestCase.java
+++ booking/src/test/java/org/sample/booking/AbstractBookingTestCase.java
@@ -17,67 +17,27 @@
 package org.sample.booking;
 
 import juzu.arquillian.Helper;
-import juzu.impl.common.Name;
-import juzu.impl.common.Tools;
-import juzu.impl.fs.spi.url.URLFileSystem;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.exporter.ZipExporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.sample.booking.qualifier.Authentication;
 
 import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.LinkedList;
 
 /** @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a> */
 @RunWith(Arquillian.class)
 public abstract class AbstractBookingTestCase {
 
   public static WebArchive createDeployment() {
-    WebArchive war = Helper.createBasePortletDeployment();
+    WebArchive war = Helper.createBasePortletDeployment(Flash.class);
     war.addAsWebInfResource(new File("src/main/webapp/WEB-INF/portlet.xml"));
-
-    //
-    try {
-      URL root = Flash.class.getClassLoader().getResource(Flash.class.getName().replace('.', '/') + ".class");
-      if (root != null) {
-        File f = new File(root.toURI()).getParentFile();
-        LinkedList<String> path = new LinkedList<String>();
-        for (String name : Name.create(Flash.class).getParent()) {
-          path.add(name);
-        }
-        add(war, f, path);
-      }
-    }
-    catch (URISyntaxException e) {
-      throw new RuntimeException(e);
-    }
-
-    //
     return war;
   }
 
   @Drone
   @Authentication
   WebDriver driver;
-
-  private static void add(WebArchive war, File f, LinkedList<String> path) {
-    if (f.isDirectory()) {
-      File[] children = f.listFiles();
-      if (children != null) {
-        for (File child : children) {
-          path.addLast(child.getName());
-          add(war, child, path);
-          path.removeLast();
-        }
-      }
-    } else {
-      war.addAsResource(f, Tools.join('/', path));
-    }
-  }
 
 }
