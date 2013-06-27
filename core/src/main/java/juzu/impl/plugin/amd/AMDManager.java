d8c925d582faecca127b630cc435b9aec293e64e
Julien Viet <julien@julienviet.com>
2013-06-27 18:43:28 +0200

    Transform string->byte[] only once

--- core/src/main/java/juzu/impl/plugin/amd/AMDManager.java
+++ core/src/main/java/juzu/impl/plugin/amd/AMDManager.java
@@ -60,8 +60,7 @@ public class AMDManager {
           if (data.isRequire) {
             resources.put(data.path, url);
           } else {
-            String content = wrap(data, url);
-            resources.put(data.path, new URL("amd", null, 0, "/", new AMDURLStreamHandler(content)));
+            resources.put(data.path, new URL("amd", null, 0, "/", new AMDURLStreamHandler(data, url)));
           }
           break;
         case SERVER :
@@ -81,113 +80,97 @@ public class AMDManager {
     return resources.get(path);
   }
 
-  private void joinDepenencies(StringBuilder sb, AMDMetaData data) {
-    for (Iterator<AMDDependency> i = data.dependencies.values().iterator(); i.hasNext();) {
-      AMDDependency dependency = i.next();
-      sb.append("'").append(dependency.name).append("'");
-      if (i.hasNext())
-        sb.append(", ");
-    }
-  }
-
-  private void joinParams(StringBuilder sb, AMDMetaData data) {
-    for (Iterator<AMDDependency> i = data.dependencies.values().iterator(); i.hasNext();) {
-      AMDDependency dependency = i.next();
-      if (dependency.alias != null && !dependency.alias.isEmpty()) {
-        sb.append(dependency.alias);
-      } else {
-        sb.append(dependency.name);
-      }
-      if (i.hasNext())
-        sb.append(", ");
-    }
-  }
-
-  private String wrap(AMDMetaData data, URL url) throws IOException {
-    StringBuilder sb = new StringBuilder();
-    sb.append("\ndefine('").append(data.name).append("', [");
-    joinDepenencies(sb, data);
-
-    sb.append("], function(");
-    joinParams(sb, data);
-
-    sb.append(") {\nvar require = Wrapper.require, requirejs = Wrapper.require,define = Wrapper.define;");
-    sb.append("\nWrapper.define.names=[");
-    joinDepenencies(sb, data);
-    sb.append("];");
-    sb.append("\nWrapper.define.deps=[");
-    joinParams(sb, data);
-    sb.append("];");
-    sb.append("\nreturn ");
-
-    int idx = -1;
-    String adapter = data.getAdapter();
-    if (adapter != null && !adapter.isEmpty()) {
-      idx = adapter.indexOf("@{include}");
-    }
-
-    // start of adapter
-    if (idx != -1) {
-      sb.append(adapter.substring(0, idx)).append("\n");
-    }
-
-    NormalizeJSReader reader = new NormalizeJSReader(new InputStreamReader(url.openStream()));
-    char[] buffer = new char[512];
-    while (true) {
-      int i = reader.read(buffer);
-      if (i == 0) {
-        continue;
-      }
-      if (i == -1) {
-        break;
-      }
-      sb.append(buffer, 0, i);
-    }
-
-    // end of adapter
-    if (idx != -1) {
-      sb.append(adapter.substring(idx + "@{include}".length(), adapter.length()));
-    }
-
-    sb.append("\n});");
-    return sb.toString();
-  }
-
   private class AMDURLStreamHandler extends URLStreamHandler {
 
     /** . */
-    private final String data;
+    private final byte[] data;
 
-    public AMDURLStreamHandler(String data) {
-      this.data = data;
+    public AMDURLStreamHandler(AMDMetaData data, URL url) throws IOException {
+
+      StringBuilder sb = new StringBuilder();
+      sb.append("\ndefine('").append(data.name).append("', [");
+      joinDependencies(sb, data);
+
+      sb.append("], function(");
+      joinParams(sb, data);
+
+      sb.append(") {\nvar require = Wrapper.require, requirejs = Wrapper.require,define = Wrapper.define;");
+      sb.append("\nWrapper.define.names=[");
+      joinDependencies(sb, data);
+      sb.append("];");
+      sb.append("\nWrapper.define.deps=[");
+      joinParams(sb, data);
+      sb.append("];");
+      sb.append("\nreturn ");
+
+      int idx = -1;
+      String adapter = data.getAdapter();
+      if (adapter != null && !adapter.isEmpty()) {
+        idx = adapter.indexOf("@{include}");
+      }
+
+      // start of adapter
+      if (idx != -1) {
+        sb.append(adapter.substring(0, idx)).append("\n");
+      }
+
+      NormalizeJSReader reader = new NormalizeJSReader(new InputStreamReader(url.openStream()));
+      char[] buffer = new char[512];
+      while (true) {
+        int i = reader.read(buffer);
+        if (i == 0) {
+          continue;
+        }
+        if (i == -1) {
+          break;
+        }
+        sb.append(buffer, 0, i);
+      }
+
+      // end of adapter
+      if (idx != -1) {
+        sb.append(adapter.substring(idx + "@{include}".length(), adapter.length()));
+      }
+
+      sb.append("\n});");
+
+      //
+      this.data = sb.toString().getBytes("UTF-8");
+    }
+
+    private void joinDependencies(StringBuilder sb, AMDMetaData data) {
+      for (Iterator<AMDDependency> i = data.dependencies.values().iterator(); i.hasNext();) {
+        AMDDependency dependency = i.next();
+        sb.append("'").append(dependency.name).append("'");
+        if (i.hasNext())
+          sb.append(", ");
+      }
+    }
+
+    private void joinParams(StringBuilder sb, AMDMetaData data) {
+      for (Iterator<AMDDependency> i = data.dependencies.values().iterator(); i.hasNext();) {
+        AMDDependency dependency = i.next();
+        if (dependency.alias != null && !dependency.alias.isEmpty()) {
+          sb.append(dependency.alias);
+        } else {
+          sb.append(dependency.name);
+        }
+        if (i.hasNext())
+          sb.append(", ");
+      }
     }
 
     @Override
     protected URLConnection openConnection(URL u) throws IOException {
-      return new AMDURLConnection(u, data);
+      return new URLConnection(u) {
+        @Override
+        public void connect() throws IOException {
+        }
+        @Override
+        public InputStream getInputStream() throws IOException {
+          return new ByteArrayInputStream(data);
+        }
+      };
     }
   }
-
-  private class AMDURLConnection extends URLConnection {
-
-    /** . */
-    private final String data;
-
-    protected AMDURLConnection(URL url, String data) {
-      super(url);
-      this.data = data;
-    }
-
-    @Override
-    public void connect() throws IOException {
-
-    }
-
-    @Override
-    public InputStream getInputStream() throws IOException {
-      ByteArrayInputStream bis = new ByteArrayInputStream(data.getBytes("UTF-8"));
-      return bis;
-    }
-  }
-
 }
