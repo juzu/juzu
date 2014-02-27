1ea17b2ecf0f0cbe6bd1609206aa3853b627e802
Julien Viet <julien@julienviet.com>
2014-02-27 10:21:08 +0100

    Minor improvement to ModuleAsset module rewriting

--- core/src/main/java/juzu/impl/plugin/amd/ModuleAsset.java
+++ core/src/main/java/juzu/impl/plugin/amd/ModuleAsset.java
@@ -1,5 +1,6 @@
 package juzu.impl.plugin.amd;
 
+import juzu.impl.common.Tools;
 import juzu.impl.plugin.asset.Asset;
 
 import java.io.ByteArrayInputStream;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.io.StringWriter;
 import java.net.URLConnection;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -22,21 +24,21 @@ public class ModuleAsset extends Asset {
   private final String adapter;
 
   /** . */
-  private final LinkedHashMap<String, String> a;
+  private final LinkedHashMap<String, String> dependencyMappings;
 
   public ModuleAsset( Map<String, Serializable> asset, String adapter, List<String> aliases) {
     super("module", asset);
 
     //
-    LinkedHashMap<String, String> a;
+    LinkedHashMap<String, String> dependencyMappings;
     if (depends != null && aliases != null) {
-      a = new LinkedHashMap<String, String>();
+      dependencyMappings = new LinkedHashMap<String, String>();
       int size = Math.min(depends.size(), aliases.size());
       for (int i = 0;i < size;i++) {
-        a.put(depends.get(i), aliases.get(i));
+        dependencyMappings.put(depends.get(i), aliases.get(i));
       }
     } else {
-      a = null;
+      dependencyMappings = null;
     }
 
     //
@@ -44,7 +46,7 @@ public class ModuleAsset extends Asset {
 
     //
     this.adapter = adapter;
-    this.a = a;
+    this.dependencyMappings = dependencyMappings;
   }
 
   @Override
@@ -52,15 +54,19 @@ public class ModuleAsset extends Asset {
 
     InputStream stream = resource.getInputStream();
 
-    if (a != null || adapter != null) {
-      StringBuilder sb = new StringBuilder();
+    if (dependencyMappings != null || adapter != null) {
+      StringWriter buffer = new StringWriter();
 
       // The define call
-      sb.append("\ndefine('").append(id).append("', [");
-      joinDependencies(sb);
-      sb.append("], function(");
-      joinParams(sb);
-      sb.append(") {");
+      buffer.append("\ndefine('").append(id).append("', [");
+      if (dependencyMappings != null) {
+        joinDependencies(buffer);
+      }
+      buffer.append("], function(");
+      if (dependencyMappings != null) {
+        joinParams(buffer);
+      }
+      buffer.append(") {");
 
       // Redeclare here define
       // Note : this only work with
@@ -69,70 +75,55 @@ public class ModuleAsset extends Asset {
       // define(?id,?dependencies,factory)
       // because we use 'arguments[2]'
       // so it should be done and tested
-      sb.append("var define = function() {");
-      sb.append("return arguments[2].apply(this, [");
-      joinParams(sb);
-      sb.append("]);");
-      sb.append("};");
+      buffer.append("var define = function() {");
+      buffer.append("return arguments[2].apply(this, [");
+      if (dependencyMappings != null) {
+        joinParams(buffer);
+      }
+      buffer.append("]);");
+      buffer.append("};");
 
-      sb.append("\nreturn ");
+      buffer.append("\nreturn ");
 
-      int idx = -1;
+      int idx;
       if (adapter != null && !adapter.isEmpty()) {
         idx = adapter.indexOf("@{include}");
+      } else {
+        idx = -1;
       }
 
-      // start of adapter
       if (idx != -1) {
-        sb.append(adapter.substring(0, idx)).append("\n");
+        buffer.append(adapter.substring(0, idx)).append("\n");
       }
-
-      NormalizeJSReader reader = new NormalizeJSReader(new InputStreamReader(stream));
-      char[] buffer = new char[512];
-      while (true) {
-        int i = reader.read(buffer);
-        if (i == 0) {
-          continue;
-        }
-        if (i == -1) {
-          break;
-        }
-        sb.append(buffer, 0, i);
-      }
-
-      // end of adapter
+      Tools.copy(new NormalizeJSReader(new InputStreamReader(stream)), buffer);
       if (idx != -1) {
-        sb.append(adapter.substring(idx + "@{include}".length(), adapter.length()));
+        buffer.append(adapter.substring(idx + "@{include}".length(), adapter.length()));
       }
 
       //
-      sb.append("\n});");
+      buffer.append("\n});");
 
       //
-      return new ByteArrayInputStream(sb.toString().getBytes());
+      return new ByteArrayInputStream(buffer.toString().getBytes());
     } else {
       return stream;
     }
   }
 
-  private void joinDependencies(StringBuilder sb) {
-    if (a != null) {
-      for (Iterator<String> i = a.keySet().iterator();i.hasNext();) {
-        sb.append("'").append(i.next()).append("'");
-        if (i.hasNext()) {
-          sb.append(", ");
-        }
+  private void joinDependencies(Appendable sb) throws IOException {
+    for (Iterator<String> i = dependencyMappings.keySet().iterator();i.hasNext();) {
+      sb.append("'").append(i.next()).append("'");
+      if (i.hasNext()) {
+        sb.append(", ");
       }
     }
   }
 
-  private void joinParams(StringBuilder sb) {
-    if (a != null) {
-      for (Iterator<String> i = a.values().iterator();i.hasNext();) {
-        sb.append(i.next());
-        if (i.hasNext()) {
-          sb.append(", ");
-        }
+  private void joinParams(Appendable sb) throws IOException {
+    for (Iterator<String> i = dependencyMappings.values().iterator();i.hasNext();) {
+      sb.append(i.next());
+      if (i.hasNext()) {
+        sb.append(", ");
       }
     }
   }
