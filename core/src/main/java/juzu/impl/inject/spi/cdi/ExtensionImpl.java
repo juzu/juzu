d62619f0cb1fc1b60ffad4400c00fd8d3b3d4890
Julien Viet <julien@julienviet.com>
2014-05-07 14:12:23 +0200

    JUZU-1 : Problem when deploying a portlet JSF CDI in the platform

--- core/src/main/java/juzu/impl/inject/spi/cdi/ExtensionImpl.java
+++ core/src/main/java/juzu/impl/inject/spi/cdi/ExtensionImpl.java
@@ -53,64 +53,71 @@ public class ExtensionImpl implements Extension {
   }
 
   <T> void processAnnotatedType(@Observes ProcessAnnotatedType<T> pat) {
+    if (manager != null) {
+      AnnotatedType<T> annotatedType = pat.getAnnotatedType();
+      Class<T> type = annotatedType.getJavaClass();
 
-    AnnotatedType<T> annotatedType = pat.getAnnotatedType();
-    Class<T> type = annotatedType.getJavaClass();
-
-    // Determine if bean should be processed
-    boolean veto = !manager.filter.accept(type);
-    if (!veto) {
-      for (AbstractBean boundBean : manager.boundBeans) {
-        Class<?> beanType = boundBean.getBeanClass();
-        if (beanType.isAssignableFrom(type)) {
-          veto = true;
-          break;
+      // Determine if bean should be processed
+      boolean veto = !manager.filter.accept(type);
+      if (!veto) {
+        for (AbstractBean boundBean : manager.boundBeans) {
+          Class<?> beanType = boundBean.getBeanClass();
+          if (beanType.isAssignableFrom(type)) {
+            veto = true;
+            break;
+          }
         }
       }
-    }
 
-    //
-    if (veto) {
-      pat.veto();
+      //
+      if (veto) {
+        pat.veto();
+      }
     }
   }
 
   void afterBeanDiscovery(@Observes AfterBeanDiscovery event, BeanManager beanManager) {
-    Container container = Container.boot.get();
+    if (manager != null) {
+      Container container = Container.boot.get();
 
-    //
-    for (Scope scope : container.scopes) {
-      if (!scope.isBuiltIn()) {
-        event.addContext(new ContextImpl(container.scopeController, scope, scope.getAnnotationType()));
+      //
+      for (Scope scope : container.scopes) {
+        if (!scope.isBuiltIn()) {
+          event.addContext(new ContextImpl(container.scopeController, scope, scope.getAnnotationType()));
+        }
       }
-    }
 
-    // Add the manager
-    event.addBean(new SingletonBean(InjectionContext.class, Tools.set(AbstractBean.DEFAULT_QUALIFIER, AbstractBean.ANY_QUALIFIER), manager));
+      // Add the manager
+      event.addBean(new SingletonBean(InjectionContext.class, Tools.set(AbstractBean.DEFAULT_QUALIFIER, AbstractBean.ANY_QUALIFIER), manager));
 
-    // Add bound beans
-    for (AbstractBean bean : manager.boundBeans) {
-      bean.register(beanManager);
-      event.addBean(bean);
+      // Add bound beans
+      for (AbstractBean bean : manager.boundBeans) {
+        bean.register(beanManager);
+        event.addBean(bean);
+      }
     }
   }
 
   void processBean(@Observes ProcessBean event, BeanManager beanManager) {
-    Bean bean = event.getBean();
-    manager.beans.add(bean);
+    if (manager != null) {
+      Bean bean = event.getBean();
+      manager.beans.add(bean);
 
-    //
-    if (bean.getScope() == Singleton.class) {
-      singletons.add(bean);
+      //
+      if (bean.getScope() == Singleton.class) {
+        singletons.add(bean);
+      }
     }
   }
 
   public void beforeShutdown(@Observes BeforeShutdown event, BeanManager beanManager) {
-    // Take care of destroying singletons
-    for (Bean singleton : singletons) {
-      CreationalContext cc = beanManager.createCreationalContext(singleton);
-      Object o = beanManager.getReference(singleton, singleton.getBeanClass(), cc);
-      singleton.destroy(o, cc);
+    if (manager != null) {
+      // Take care of destroying singletons
+      for (Bean singleton : singletons) {
+        CreationalContext cc = beanManager.createCreationalContext(singleton);
+        Object o = beanManager.getReference(singleton, singleton.getBeanClass(), cc);
+        singleton.destroy(o, cc);
+      }
     }
   }
 }
