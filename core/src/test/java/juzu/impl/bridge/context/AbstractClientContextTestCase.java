24a9b49c14cd71d8ffe9cfe9c4d2870f0b383030
Tran The Trong <trongtt@gmail.com>
2014-06-11 17:27:23 +0700

    JUZU-3 Fix the ERROR in PortletClientContextTestCase

--- core/src/test/java/juzu/impl/bridge/context/AbstractClientContextTestCase.java
+++ core/src/test/java/juzu/impl/bridge/context/AbstractClientContextTestCase.java
@@ -16,16 +16,22 @@
 
 package juzu.impl.bridge.context;
 
-import junit.framework.Assert;
 import juzu.test.AbstractWebTestCase;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.DefaultRedirectStrategy;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 /** @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a> */
@@ -57,20 +63,38 @@ public abstract class AbstractClientContextTestCase extends AbstractWebTestCase 
     contentType = null;
     content = null;
     URL url = new URL(link.getAttribute("href"));
-    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-    conn.setDoInput(true);
-    conn.setDoOutput(true);
-    conn.setRequestProperty("Content-type", "application/octet-stream;charset=UTF8");
-    conn.connect();
-    OutputStream out = conn.getOutputStream();
-    OutputStreamWriter writer = new OutputStreamWriter(out, "UTF-8");
-    writer.write("foo");
-    writer.flush();
-    Assert.assertEquals(200, conn.getResponseCode());
-    assertEquals(3, contentLength);
-    assertEquals("UTF8", charset);
-    assertEquals("application/octet-stream;charset=UTF8", contentType);
-    assertEquals("foo", content);
-    assertEquals(kind, AbstractClientContextTestCase.kind);
+
+    DefaultHttpClient client = new DefaultHttpClient();
+    client.setRedirectStrategy(new DefaultRedirectStrategy() {                
+        public boolean isRedirected(HttpRequest request, HttpResponse response, HttpContext context)  {
+            boolean isRedirect=false;
+            try {
+                isRedirect = super.isRedirected(request, response, context);
+            } catch (ProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            if (!isRedirect) {
+                int responseCode = response.getStatusLine().getStatusCode();
+                if (responseCode == 301 || responseCode == 302) {
+                    return true;
+                }
+            }
+            return isRedirect;
+        }});
+
+    try {
+        HttpPost post = new HttpPost(url.toURI());
+        post.setEntity(new StringEntity("foo", HTTP.OCTET_STREAM_TYPE, HTTP.UTF_8));
+        HttpResponse response = client.execute(post);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+        assertEquals(3, contentLength);
+        assertEquals("UTF-8", charset);
+        assertEquals("application/octet-stream; charset=UTF-8", contentType);
+        assertEquals("foo", content);
+        assertEquals(kind, AbstractClientContextTestCase.kind);
+    } finally {
+        client.getConnectionManager().shutdown();
+    }
   }
 }
