906521743d47cb473bdee7f1581f5ba5451d5a4b
phuong_vu <phuong.vu@exoplatform.com>
2014-10-22 15:11:26 +0700

    JUZU-12 Juzu weather7 and weather8 portlet doesn't work well with GateIn

--- doc/tutorial/examples/src/main/java/examples/tutorial/weather8/assets/weather.js
+++ doc/tutorial/examples/src/main/java/examples/tutorial/weather8/assets/weather.js
@@ -1,9 +1,11 @@
+$.noConflict();
+$ = $ || jQuery;
+
 $(function () {
 
-  // Setup an event listener on the .collapse element when it is shown
-  $('.collapse').on('show', function () {
-
-    // Get the location attribute from the dom
+	// Setup an event listener on the .collapse element when it is shown
+  $(document).off('show').on('show', '.collapse', function() {
+	// Get the location attribute from the dom
     var location = $(this).attr("id");
 
     // Update the .accordion-inner fragment
@@ -11,15 +13,21 @@ $(function () {
       closest(".accordion-group").
       find(".accordion-inner").
       each(function () {
-
+    	  
       // Load the fragment from the resource controller
       $(this).jzLoad(
         "Weather.getFragment()",
         {"location":location});
-
     });
   });
-
-  // Setup the collapse component
-  $(".collapse").collapse();
+	
+  $(document).off('click', '.accordion-toggle').on('click', '.accordion-toggle', function() {
+	  if (!$(this).data('weather8')) {
+		  $(this).closest('.accordion-group').find('.collapse').collapse();
+		  $(this).data('weather8', true);
+	  } else {
+		  $(this).closest('.accordion-group').find('.collapse').collapse('toggle');
+	  }
+	  return false;
+  });
 });
